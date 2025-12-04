d8a104724e45d6ae1a7fd948544c434ea595b17d
Ashrithabalusumilli <ashrithabalusumilli@gmail.com>
2025-12-04 15:21:12 +0530

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,u,t,x;
+	printf("enter a u t values");
+	scanf("%d%d%d",&a,&u,&t);
+	x=u*t+(a*t*t)/2;
+	printf("distance is:%d",x);
+	return 0;
+}
