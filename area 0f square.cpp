eb5eb58fa94f6d5fe465e3ece9789d99006a5c89
Ashrithabalusumilli <ashrithabalusumilli@gmail.com>
2025-12-04 09:29:43 +0530

    Add files via upload

---
+++ area 0f square.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int side;
+ float area;
+ printf("enter side value");
+ scanf("%d",&side);
+ area=side*side;
+ printf("area of square is:%f",area);
+ return 0;
+
+}
